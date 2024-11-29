a7b226b82ef07a95e953891f0f9fe07a2c686212
Xcool <92432210+XCO0@users.noreply.github.com>
2024-11-30 01:02:00 +0800

    Site updated: 2024-11-30 01:01:58

--- 2024/11/29/carbonx/process.py
+++ 2024/11/29/carbonx/process.py
@@ -1,23 +1,23 @@
-# import os
+import os
 
-# # 获取当前文件夹路径
-# current_folder = os.getcwd()
+# 获取当前文件夹路径
+current_folder = os.getcwd()
 
-# # 遍历当前文件夹下所有 .jpg 文件
-# for filename in os.listdir(current_folder):
-#     if filename.endswith(".jpg"):
-#         # 使用 "_" 分割文件名并获取最后一部分
-#         new_name = filename.split("_")[-1]
+# 遍历当前文件夹下所有 .jpg 文件
+for filename in os.listdir(current_folder):
+    if filename.endswith(".jpg"):
+        # 使用 "_" 分割文件名并获取最后一部分
+        new_name = filename.split("_")[-1]
 
-#         # 构造旧文件的完整路径和新文件的完整路径
-#         old_path = os.path.join(current_folder, filename)
-#         new_path = os.path.join(current_folder, new_name)
+        # 构造旧文件的完整路径和新文件的完整路径
+        old_path = os.path.join(current_folder, filename)
+        new_path = os.path.join(current_folder, new_name)
 
-#         # 重命名文件
-#         os.rename(old_path, new_path)
+        # 重命名文件
+        os.rename(old_path, new_path)
 
-#         print(f'Renamed: {filename} -> {new_name}')
+        print(f'Renamed: {filename} -> {new_name}')
 
-for i in range(20):
+for i in range(22):
     name = "0" + str(i) if i < 10 else str(i)
     print("{%asset_image "+name+".jpg%}")
